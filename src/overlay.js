596445a280ff07323abdee8ceac7169127360b65
Your Name <noelgalan999@gmail.com>
2025-04-11 09:51:51 +1000

    new branch

---
+++ src/overlay.js
@@ -0,0 +1,5 @@
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { BloomPass } from 'three/addons/postprocessing/BloomPass.js';
+import { FilmPass } from 'three/addons/postprocessing/FilmPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
