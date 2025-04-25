901e547ff1aa274ddfe5a176dd57e2c83b352419
Your Name <noelgalan999@gmail.com>
2025-04-25 17:27:22 +1000

    I tyhink that should be good just gotta see whaat to do withg cannondebugger since tis broken

---
+++ src/Effects.js
@@ -0,0 +1,37 @@
+/*import * as THREE from 'three';
+
+            import {EffectComposer, OutlinePass, FilmPass, BloomPass, } from 'three/addons';
+
+            export const composer = new EffectComposer(renderer);
+
+            const renderPass = new RenderPass(scene, camera);
+            composer.addPass(renderPass);
+
+            const bloomPass = new BloomPass(
+                5, 25, 4, 256
+            );
+
+            const filmPass = new FilmPass(
+                0.5,
+                false
+            );
+
+            export const outlinePass = new OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight),
+                scene, camera);
+            outlinePass.edgeStrength = 3;
+            outlinePass.edgeThickness = 1;
+            outlinePass.visibleEdgeColor.set(0xffff00);
+            composer.addPass(outlinePass);
+
+            composer.addPass(bloomPass);
+            class Effects{
+                
+                
+            } */
+
+
+
+    
+
+
+
