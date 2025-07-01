a4b61dd0ec3b5d8e4e41468d735090b71589b642
shernjr <38554378+shernjr@users.noreply.github.com>
2025-07-01 11:43:37 +1000

    testing adding a bunch of furniture
    test adding a bunch of furniture in one spot

--- src/Maze.js
+++ src/Maze.js
@@ -117,6 +117,7 @@ class Maze {
         this.generateEntities(this.entityTypes.get('exitDoor'), 30, 30, 10);
         this.generateEntities(this.entityTypes.get('powerSwitch'), 250, 10, 10);
         this.generateEntities(this.entityTypes.get('furniture'), 750, 4, 2, {onWall: false}); // NEW CODE
+        this.generateEntities(this.entityTypes.get('furniture'), 40, 1, 1, {onWall: false}); // NEW CODE, bunch of furniture together
 
         this.entities.forEach(arr => arr.forEach(({cell}) => this.createPath(this.origin(), cell)));
 
